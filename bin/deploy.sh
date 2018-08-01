632b505ec1496398d7fdad04ffe24cac80e20374
Vlad Bokov <razum2um@mail.ru>
2018-08-02 06:12:43 +0700

    optimize static

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,8 @@
-JEKYLL_ENV=production octopod build && octopod deploy
+#!/bin/bash
+set -euxo pipefail
+
+JEKYLL_ENV=production octopod build
+find _site -type f -iname '*js' -exec gzip -9 -k -f {} \;
+find _site -type f -iname '*css' -exec gzip -9 -k -f {} \;
+find _site -type f -iname '*svg' -exec gzip -9 -k -f {} \;
+octopod deploy
