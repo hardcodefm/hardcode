1da7849ed437b59ce7e6a984fd93ea9c9847514a
Vlad Bokov <razum2um@mail.ru>
2018-10-01 05:11:29 +0700

    Episode 3

--- _plugins/hardcode-author.rb
+++ _plugins/hardcode-author.rb
@@ -3,7 +3,8 @@ module HardcodeAuthor
   AUTHOR_LINKS = {
     'Владимир' => 'https://github.com/razum2um',
     'Иван' => 'https://github.com/shredder-rull',
-    'Денис' => 'https://github.com/zloirock'
+    'Денис' => 'https://github.com/zloirock',
+    'Андрей' => 'https://github.com/andrewzhurov'
   }
 
   def hardcode_author_link(name)
