ca9e9c1d9ead9798d954324525eb801c6db74094
Vlad Bokov <razum2um@mail.ru>
2018-10-15 03:55:49 +0700

    Use zopfli instead of gzip

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 set -euxo pipefail
 
+checkinstall() {
+    if test ! $(which $1); then
+        if test ! $(which brew); then
+            echo "$1 not installed"
+            exit 1
+        fi
+        brew install $1
+    fi
+}
+
+checkinstall zopfli
+checkinstall brotli
+
 JEKYLL_ENV=production octopod build
-find _site -type f -iname '*js' -exec gzip -9 -k -f {} \;
+find _site -type f -iname '*js' -exec zopfli -i1000 {} \;
 find _site -type f -iname '*js' -exec brotli -9 -k -f {} \;
 
-find _site -type f -iname '*css' -exec gzip -9 -k -f {} \;
+find _site -type f -iname '*css' -exec zopfli -i1000 {} \;
 find _site -type f -iname '*css' -exec brotli -9 -k -f {} \;
 
-find _site -type f -iname '*svg' -exec gzip -9 -k -f {} \;
+find _site -type f -iname '*svg' -exec zopfli -i1000 {} \;
 find _site -type f -iname '*svg' -exec brotli -9 -k -f {} \;
 
-octopod deploy
+#octopod deploy
